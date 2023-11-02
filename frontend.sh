7e043c95b53d27e94f25ca578e9a63c3decfec29
Anto <japanto20@gmail.com>
2023-11-02 15:43:36 -0500

    Create servers

--- frontend.sh
+++ frontend.sh
@@ -4,16 +4,16 @@ dnf install nginx -y &>>/tmp/expense.log
 echo -e "\e[36m Copying expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
-echo -e "\e[36m  Clean Old Nginx Content \e[0m"
+echo -e "\e[36m Clean Old Nginx Content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
-echo -e "\e[36m  Download Frontend Application Code \e[0m"
+echo -e "\e[36m Download Frontend Application Code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
-echo -e "\e[36m  Extract Downloaded Application Content \e[0m"
+echo -e "\e[36m Extract Downloaded Application Content \e[0m"
 cd /usr/share/nginx/html &>>/tmp/expense.log
 unzip /tmp/frontend.zip &>>/tmp/expense.log
 
-echo -e "\e[36m  Starting Nginx Service \e[0m"
+echo -e "\e[36m Starting Nginx Service \e[0m"
 systemctl enable nginx &>>/tmp/expense.log
 systemctl restart nginx &>>/tmp/expense.log
