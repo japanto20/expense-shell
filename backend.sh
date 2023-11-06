f08572f353984909e51857840df29c743d833520
Anto <japanto20@gmail.com>
2023-11-06 09:45:39 -0600

    Create servers

--- backend.sh
+++ backend.sh
@@ -6,67 +6,65 @@ if [ -z "$1" ]; then
 fi
 MYSQL_ROOT_PASSWORD=$1
 
-
-
 echo -e "${color} Disable NodeJS default Version \e[0m"
-dnf module disable nodejs -y &>>$log_file
+dnf module disable nodejs -y &>>"$log_file"
  status_check
 
 echo -e "${color} Enable NodeJS 18 Version \e[0m"
-dnf module enable nodejs:18 -y &>>$log_file
+dnf module enable nodejs:18 -y &>>"$log_file"
  status_check
 
 echo -e "${color} Install NodeJS \e[0m"
-dnf install nodejs -y &>>$log_file
+dnf install nodejs -y &>>"$log_file"
  status_check
 
 echo -e "${color} Copy Backend Service File \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
  status_check
 
-id expense &>>$log_file
+id expense &>>"$log_file"
 if [ $? -ne 0 ]; then
   echo -e "${color} Add Application User \e[0m"
-  useradd expense &>>$log_file
+  useradd expense &>>"$log_file"
  status_check
 fi
 
 
 echo -e "${color} Create Application Directory \e[0m"
-mkdir -p /app &>>$log_file
+mkdir -p /app &>>"$log_file"
  status_check
 
 
 echo -e "${color} Delete old Application Content \e[0m"
-rm -rf /app/* &>>$log_file
+rm -rf /app/* &>>"$log_file"
 status_check
 
 echo -e "${color} Download Application Content \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>"$log_file"
 status_check
 
 echo -e "${color} Extract Application Content \e[0m"
-cd /app &>>$log_file
-unzip /tmp/backend.zip &>>$log_file
+cd /app &>>"$log_file"
+unzip /tmp/backend.zip &>>"$log_file"
 status_check
 
 
 echo -e "${color} Download NodeJS Dependencies \e[0m"
-npm install &>>$log_file
+npm install &>>"$log_file"
 status_check
 
 echo -e "${color} Install MySQL Client to Load Schema \e[0m"
-dnf install mysql -y &>>$log_file
+dnf install mysql -y &>>"$log_file"
 status_check
 
 echo -e "${color} Load Schema \e[0m"
-mysql -h mysql-dev.antodevops20.online -uroot -p"${MYSQL_ROOT_PASSWORD}" < /app/schema/backend.sql &>>$log_file
+mysql -h mysql-dev.antodevops20.online -uroot -p"${MYSQL_ROOT_PASSWORD}" < /app/schema/backend.sql &>>"$log_file"
 status_check
 
 echo -e "${color} Starting Backend Service \e[0m"
-systemctl daemon-reload &>>$log_file
+systemctl daemon-reload &>>"$log_file"
 status_check
 
-systemctl enable backend &>>$log_file
-systemctl start backend &>>$log_file
+systemctl enable backend &>>"$log_file"
+systemctl start backend &>>"$log_file"
 status_check
--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,28 @@
-log_file=/tmp/expense.log
-color="\e[33m"
+source common.sh
+
+if [ -z "$1" ]; then
+  echo -e "\e[31m Password Input Missing \e[0m"
+  exit
+fi
+MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${color} Disable MySQL default Version \e[0m"
-dnf module disable mysql -y &>>$log_file
-echo $?
+dnf module disable mysql -y &>>"$log_file"
+status_check
 
 echo -e "${color} Copy MySQL Repo file \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>"$log_file"
+status_check
 
 echo -e "${color} Install MySQL Server \e[0m"
-dnf install mysql-community-server -y &>>$log_file
-echo $?
+dnf install mysql-community-server -y &>>"$log_file"
+status_check
 
 echo -e "${color} Start MySQL Server \e[0m"
-systemctl enable mysqld &>>$log_file
-systemctl start mysqld &>>$log_file
-echo $?
+systemctl enable mysqld &>>"$log_file"
+systemctl start mysqld &>>"$log_file"
+status_check
 
 echo -e "${color} Set MySQL Password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+mysql_secure_installation --set-root-pass "${MYSQL_ROOT_PASSWORD}" &>>"$log_file"
+status_check
